a126a2e5df1362fc4b212fc53900d49452b354cd
SUBHASHIS <subhashis.sarangi@gmail.com>
2024-02-02 19:20:26 +0530

    skeleton project

---
+++ src/main/java/com/myapp/AzureJavaAppService1/AzureJavaAppService1Application.java
@@ -0,0 +1,13 @@
+package com.myapp.AzureJavaAppService1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureJavaAppService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureJavaAppService1Application.class, args);
+	}
+
+}
